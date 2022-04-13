eb66684f7090d7757e037a2f7eb5c13178b07b74
bee <rkddbtjd123@gmail.com>
2022-04-13 18:12:56 +0900

    feat: 날씨 정보에서 풍속 제거

--- src/main/kotlin/us/util/maldiveswhether/application/out/MaldivesWhetherDto.kt
+++ src/main/kotlin/us/util/maldiveswhether/application/out/MaldivesWhetherDto.kt
@@ -2,10 +2,9 @@ package us.util.maldiveswhether.application.out
 
 import java.time.LocalTime
 
-class MaldivesWhetherDto(
+data class MaldivesWhetherDto(
     val time: LocalTime,
     val temp: Int,
-    val windSpeed: Int,
     val humidity: Int,
     val barometer: Int,
     val whether: String
--- src/main/kotlin/us/util/maldiveswhether/domain/Whether.kt
+++ src/main/kotlin/us/util/maldiveswhether/domain/Whether.kt
@@ -5,7 +5,6 @@ import java.time.LocalTime
 class Whether(
     val time: LocalTime,
     val temp: Int,
-    val windSpeed: Int,
     val humidity: Int,
     val barometer: Int,
     val whether: String
