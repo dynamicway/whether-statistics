8c9494e9f5abc1e13747cac58480ab31d9037e3a
bee <rkddbtjd123@gmail.com>
2022-04-13 17:12:47 +0900

    feat: 몰디브 날씨 정보가 담긴 문자열을 MaldivesWhetherDto 로 변환해주는 객체 초기화

---
+++ src/main/kotlin/us/util/maldiveswhether/adaptor/out/MaldivesWhetherDtoMapper.kt
@@ -0,0 +1,13 @@
+package us.util.maldiveswhether.adaptor.out
+
+import org.springframework.stereotype.Component
+import us.util.maldiveswhether.application.out.MaldivesWhetherDto
+
+@Component
+class MaldivesWhetherDtoMapper {
+
+    fun mapByString(whetherString: String): List<MaldivesWhetherDto> {
+        TODO()
+    }
+
+}
